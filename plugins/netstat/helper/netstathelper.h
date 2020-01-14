aa27b334ccda43618869f105cf45db5a8726a9a0
Tomaz Canabrava <tcanabrava@kde.org>
2020-01-14 18:10:09 +0000

    Add support for SS besides Netstat
    This adds preliminary support for SS besides Netstat
    the header is wrong, but not hard to fix

--- plugins/netstat/helper/netstathelper.h
+++ plugins/netstat/helper/netstathelper.h
@@ -16,7 +16,19 @@ public Q_SLOTS:
 
 private:
     QVariantList parseOutput(const QByteArray &netstatOutput);
+    QVariantList parseNetstatOutput(const QByteArray &netstatOutput);
+    QVariantList parseSSOutput(const QByteArray &ss);
+
     QString extractAndStrip(const QString &src,const int &index, const int  &size);
+
+    /* Netstat has been deprecated for more than 20 years,
+    * some distros such as arch linux use 'ss' as default.
+    */
+    int mHasSS;
+
+    /* Distros are not obliged to install this. let's query it before
+    * assuming that this actually exists */
+    int mHasNetstat;
 };
 
 #endif // NETSTATHELPER_H
